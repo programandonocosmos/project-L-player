bb42c84e062fd78a6b61ec41787c4c2cbd7c69b9
Davi Suga <daviciencia1@gmail.com>
2022-12-27 15:51:41 -0300

    feat: added json serializer for game state

---
+++ game_adapter.py
@@ -0,0 +1,31 @@
+from functools import reduce
+import json
+from typing import Dict, Tuple, TypeVar, cast
+
+from projectl import VisibleState
+
+T = TypeVar("T")
+
+
+def merge_tuple_tuple_in_dict(
+    accumulator: Dict[T, Dict[T, T]],
+    item: Tuple[Tuple[T, T], T],
+) -> Dict[T, Dict[T, T]]:
+    ((k1, k2), value) = item
+    return {
+        **accumulator,
+        k1: {**(accumulator.get(k1) or {}), k2: value},
+    }
+
+
+def json_of_game_state(game_state: VisibleState) -> str:
+    return json.dumps(
+        {
+            **game_state,
+            "players_pieces": reduce(
+                merge_tuple_tuple_in_dict,
+                game_state["players_pieces"].items(),
+                cast(Dict[int, Dict[int, int]], {}),
+            ),
+        }
+    )
